1134cef4a31ef4f78837d194427e0a714f626057
girelaignacio <girelaignacio@gmail.com>
2023-08-21 12:19:03 -0300

    add namespace

--- R/RandomKfoldCV.Model.R
+++ R/RandomKfoldCV.Model.R
@@ -1,24 +1,25 @@
+#' @export
 RandomKfoldCV.Model <- function(target, which.dataframe){
-  data <- select_dataframes(which.dataframe) 
-  
+  data <- select_dataframes(which.dataframe)
+
   # split data
-  
+
   data <- random.split(data, 0.8)
   dataTrain <- data$dataTrain
   dataTest <- data$dataTest
-  
+
   # Retrieve only the target variable and predictors
   ytrain <- dataTrain[,target]
   ytest <- dataTest[,target]
   dataTrain <- cbind(ytrain, dataTrain[,-c(1:13)])
   dataTest <- cbind(ytest, dataTest[,-c(1:13)])
-  
-  
+
+
   selected.models <- KfoldCV.RandomSearch(dataTrain, nfolds = 5)
-  
+
   ypreds <- modelPredictions(dataTest, selected.models)
-  
+
   RMSE <- apply(as.data.frame(ypreds), MARGIN = 2, FUN = function(x) caret::RMSE(x,ytest))
-  
+
   return(RMSE)
 }
