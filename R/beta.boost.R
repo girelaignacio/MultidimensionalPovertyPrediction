98a79bd61061b5a877406b123908d15029e40c14
Ignacio Girela <girelaignacio@gmail.com>
2024-05-26 17:33:34 -0300

    make big changes

--- R/beta.boost.R
+++
@@ -1,6 +0,0 @@
-beta.boost <- function(X, y) {
-  data <- cbind.data.frame(X,y)
-  modelframe <- model.frame(y ~ . ,data)
-  betaboost <- mboost::glmboost(y~., data, family = betaboost::BetaReg())
-  betaboost
-}
--- R/random.split.R
+++
@@ -1,6 +0,0 @@
-random.split <- function(df, train_size){
-  ids <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(train_size,1-train_size))
-  train <- df[ids,]
-  test <- df[!ids,]
-  return(list(dataTrain = train, dataTest = test))
-}
